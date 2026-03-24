7f299d942e10647f586181828bdab63e1d60fbee
ray.park@teammover.co.kr <rayparkinteammover>
2026-03-24 23:11:49 +0900

    firebase 경로 수정

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { db, auth, provider } from "./firebase";
+import { db, auth, provider } from "../firebase";
 import {
   collection,
   addDoc,
